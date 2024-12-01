92a7efda049cb964329a7cbb1d8401d75f411879
Aidan Wolk <aidanwolk@comcast.net>
2024-11-30 21:12:53 -0800

    Implement day 1

---
+++ ruby/day1.rb
@@ -0,0 +1,16 @@
+require_relative './lib/aoc'
+require_relative './lib/parser'
+
+input = AOC.get_input(1)
+parser = P.int.delimited('   ').each_line
+pairs = parser.parse_all(input)
+
+left = pairs.map(&:first).sort!
+right = pairs.map(&:last).sort!
+
+pt1 = left.zip(right).sum {|a, b| (a - b).abs}
+puts   "Part 1: #{pt1}"
+
+right_counts = right.tally
+pt2 = left.sum {|n| n * right_counts.fetch(n, 0)}
+puts "Part 2: #{pt2}"
