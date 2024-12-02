d5af2dd7a02943b76339e0c520bf452511d7b46a
Aidan Wolk <aidanwolk@comcast.net>
2024-12-01 21:14:15 -0800

    Implement day 2

---
+++ ruby/day2.rb
@@ -0,0 +1,24 @@
+require_relative './lib/aoc'
+require_relative './lib/parser'
+
+input = AOC.get_input(2)
+reports = P.int.delimited(' ').each_line.parse_all(input)
+
+def safe?(report)
+  diffs = report.each_cons(2).map {|a, b| a - b}
+  return false unless diffs.all?(&:positive?) || diffs.all?(&:negative?)
+  diffs.all? {|i| (1..3).include?(i.abs)}
+end
+
+pt1 = reports.count do |report|
+  safe?(report)
+end
+puts "Part 1: #{pt1}"
+
+pt2 = reports.count do |report|
+  next true if safe?(report)
+  (0...report.length).any? do |index|
+    safe?(report.values_at(0...index, (index+1)...report.length))
+  end
+end
+puts "Part 2: #{pt2}"
