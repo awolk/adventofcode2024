42c1c5577f9819e6234ab19847266f4672fa859d
Aidan Wolk <aidanwolk@comcast.net>
2024-12-12 22:06:55 -0800

    Clean up day 13

--- ruby/day13.rb
+++ ruby/day13.rb
@@ -1,47 +1,33 @@
 require_relative './lib/aoc'
 require_relative './lib/parser'
 
-Machine = Data.define(:ax, :ay, :bx, :by, :px, :py)
+Machine = Data.define(:ax, :ay, :bx, :by, :px, :py) do
+  def tokens
+    # px = a * ax + b * bx
+    # py = a * ay + b * by
+    a = (px - py * bx / by) / (ax - ay * bx / by)
+    b = (py - a * ay) / by
 
+    return nil if a != a.to_i || b != b.to_i
+    (a * 3 + b).to_i
+  end
+
+  def corrected_prize = with(px: px + 10000000000000, py: py + 10000000000000)
+end
+
+rat_p = P.int.map(&:to_r)
 machine_parser = P.seq(
-  "Button A: X+", P.int, ", Y+", P.int,
-  "\nButton B: X+", P.int, ", Y+", P.int,
-  "\nPrize: X=", P.int, ", Y=", P.int
+  "Button A: X+", rat_p, ", Y+", rat_p,
+  "\nButton B: X+", rat_p, ", Y+", rat_p,
+  "\nPrize: X=", rat_p, ", Y=", rat_p
 ).map {|vals| Machine.new(*vals)}
 parser = machine_parser.delimited("\n\n")
 
 input = AOC.get_input(13)
-# input = AOC.get_example_input(13)
 machines = parser.parse_all(input)
 
-pt1 = machines.sum do |m|
-  # px = a * ax + b * bx
-  # py = a * ay + b * by
-  # where minimum a
-  
-  # b = (py -  a * ay) / by
-  # px = a * ax + (py -  a * ay) * bx/by
-  # px = a*ax + py*bx/by - a*ay*bx/by
-  # a * (ax - ay * bx / by) = px - py*bx/by
-  # a = (px - py*bx/by) / (ax - ay * bx / by)
-  a = (m.px - m.py * m.bx.to_f / m.by) / (m.ax - m.ay * m.bx.to_f / m.by)
-  b = (m.py - a * m.ay) / m.by
-
-  next 0 if (a - a.round).abs > 0.00001 || (b - b.round).abs > 0.00001
-
-  a.round * 3 + b.round
-end
+pt1 = machines.filter_map(&:tokens).sum
 puts "Part 1: #{pt1}"
 
-pt2 = machines.sum do |m|
-  px = 10000000000000 + m.px.to_f
-  py = 10000000000000 + m.py.to_f
-  a = (px - py * m.bx.to_f / m.by) / (m.ax - m.ay * m.bx.to_f / m.by)
-  b = (py - a * m.ay) / m.by
-
-  # this level of precision seems arbitrary...
-  next 0 if (a - a.round).abs > 0.001 || (b - b.round).abs > 0.001
-
-  a.round * 3 + b.round
-end
+pt2 = machines.map(&:corrected_prize).filter_map(&:tokens).sum
 puts "Part 2: #{pt2}"
